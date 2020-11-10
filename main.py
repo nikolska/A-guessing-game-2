2150f3d86cf1c4e6e9de699aca9b2e7c2bf86f75
nikolska <nikolska1995@gmail.com>
2020-11-10 16:05:27 +0100

    last one

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import time
 
 
 def guess():
-    print('Choose number between 0 and 1000')
+    '''
+    Function guess checks the expected user number, replacing the max or min limits
+    of the possibility of finding the number.
+
+    :min_num: minimum border, type - int
+    :max_num: maximum border, type - int
+    :attempts: number of computer attempts, type - int
+    :guessing: computer number selection algorithm, type - int
+    :user_answer: the user's answer, entered through the keyboard, type - string
+    '''
+    print('Choose number between 0 and 1000 and i will try to guess it')
     time.sleep(5)
     min_num = 0
     max_num = 1000
+    attempts = 0
     while True:
         guessing = int((max_num - min_num) / 2) + min_num
         print('I guess your number is:', guessing)
-        x = input('Too big, too small or guessed? ').lower()
-        if x == 'guessed':
-            print('I guess your number!')
+        user_answer = input('Too big, too small or guessed? ').lower()
+        if user_answer == 'guessed':
+            print(f'I win! I guessed your number in {attempts} tries!')
             break
         else:
-            if x == 'too big':
+            if user_answer == 'too big':
                 max_num = guessing
+                attempts += 1
             else:
-                if x == 'too small':
+                if user_answer == 'too small':
                     min_num = guessing
+                    attempts += 1
                 else:
                     print('Do not cheat!')
 
